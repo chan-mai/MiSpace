c67812d297e3c2c2bd3a14332c41aacf62099fa9
chan-mai <yashinomi0804@gmail.com>
2024-07-26 16:30:38 +0900

    add: tailwindcss, prelineの追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: {
+    enabled: true
+  },
+  css: ['~/assets/css/base.css'],
+  plugins: ["~/plugins/preline.client.ts"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue",
+    "./error.vue",
+    './node_modules/preline/preline.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('preline/plugin'),
+  ],
+}
+
