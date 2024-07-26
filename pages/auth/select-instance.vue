bd5e85c9eb8ac4c918fbc15561bbebb196b1646b
taichanne30 <dev@taichan.site>
2024-07-27 06:51:49 +0900

    まちがえました

--- pages/auth/select-instance.vue
+++ pages/auth/select-instance.vue
@@ -73,7 +73,7 @@ const login = (instance: string | undefined) => {
                     <form @submit.prevent="login(instance)">
                         <div class="mb-4">
                         <label for="hs-hero-password-2" class="block text-sm font-medium dark:text-white"><span class="sr-only">Password</span></label>
-                        <input type="text" v-model="instance" autocomplete="on" class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" placeholder="インスタンスURL" required>
+                        <input type="text" v-model="instance" class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" placeholder="インスタンスURL" required>
                         </div>
 
                         <div class="grid">
